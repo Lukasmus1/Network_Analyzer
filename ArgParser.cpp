d3fcce697a8c0502a67c1b09508cbd0bb6c833a4
Lukáš Píšek <lukin.pisek@email.cz>
2024-09-21 14:34:24 +0200

    feat: arg parsing now a seperate class

---
+++ ArgParser.cpp
@@ -0,0 +1,74 @@
+﻿#include "ArgParser.h"
+#include <cstring>
+
+ArgParser::ArgParser(int argc, char* argv[], std::string* interface, std::string* sort_by)
+{
+    _argc = argc;
+    _argv.assign(argv, argv + argc); 
+    _interface = interface;
+    _sort_by = sort_by;
+}
+
+ArgParser::~ArgParser() 
+{
+    _argv.clear();
+}
+
+std::string ArgParser::check_arg(const int i)
+{
+    if (i + 1 < _argc)
+    {
+        return _argv[i + 1];
+    }
+    
+    return "ERROR";
+}
+
+
+int ArgParser::parse()
+{
+    //==========ARGUMENT PARSING==========
+    for (int i = 1; i < _argc; i++)
+    {
+        if (_argv[i] == "-i")
+        {
+            *_interface = check_arg(i);
+            i++; 
+        }
+        else if (_argv[i] == "-s")
+        {
+            *_sort_by = check_arg(i);
+            i++;
+        }
+        else
+        {
+            std::cerr << "Wrong arguments\n";
+            return 1;
+        }
+    }
+
+    //==========ERROR CHECKING==========
+    
+    //Interface check
+    if (_interface->empty())
+    {
+        std::cerr << "You must choose an interface!\n";
+        return 1;
+    }
+    
+    //If interface or sort_by is ERROR, crash.
+    if (strcmp(_interface->c_str(), "ERROR") == 0 || strcmp(_sort_by->c_str(), "ERROR") == 0)
+    {
+        std::cerr << "Wrong arguments\n";
+        return 1;
+    }
+    
+    //If sort_by is not empty and not "p" or "b", crash.
+    if(strcmp(_sort_by->c_str(), "p") != 0 && strcmp(_sort_by->c_str(), "b") != 0 && !_sort_by->empty())
+    {
+        std::cerr << "Argument '-s' must be followed by either 'p' or 'b'\n";
+        return 1;
+    }
+
+    return 0;
+}
---
+++ ArgParser.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include <iostream>
+#include <vector>
+
+class ArgParser
+{
+public:
+    ArgParser(int argc, char* argv[], std::string* interface, std::string* sort_by);
+    ~ArgParser();
+    int parse();
+
+private:
+    std::string check_arg(int i);
+    int _argc;
+    std::vector<std::string> _argv;
+    std::string* _interface;
+    std::string* _sort_by;
+};
